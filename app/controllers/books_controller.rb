4dce8d0882c414ffc46a2d553557706d1b21358d
itchiness386 <itchiness386@gmail.com>
2020-01-12 12:01:51 +0900

    [Add] 投稿詳細画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,10 @@ class BooksController < ApplicationController
   def index
   	@books = Book.all
   	@book = Book.new
-
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def create
